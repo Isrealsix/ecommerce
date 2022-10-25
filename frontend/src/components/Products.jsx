d086755e94e8f5d441123eb0d58c101db7e642f0
Isreal6ix <isrealsix@gmail.com>
2022-10-25 19:18:52 +0700

    added rating stars

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Products = ({ product }) => {
 	return (
@@ -14,14 +15,13 @@ const Products = ({ product }) => {
 					</Card.Title>
 				</a>
 				<Card.Text as="div">
-					<div className="my-3">
-						{product.rating} from {product.numReviews} reviews
-					</div>
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+					/>
 				</Card.Text>
 
-				<Card.Text as='h3'>
-					${product.price}
-				</Card.Text>
+				<Card.Text as="h3">${product.price}</Card.Text>
 			</Card.Body>
 		</Card>
 	);
