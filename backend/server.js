93f6a1f905f9892c9ed260dfc63ac5ce96e9d571
Isreal6ix <isrealsix@gmail.com>
2022-10-26 16:05:58 +0700

    configured and used dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const { products } = require('./data/products');
 const app = express();
+
+dotenv.config();
 
 app.get('/', (req, res) => {
 	res.send('API online')
@@ -15,4 +18,5 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product);
 })
 
-app.listen(5000, console.log('Server running on PORT 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`));
