e495a66a60489b93c386cdcae50c97b3c6511f89
Isreal6ix <isrealsix@gmail.com>
2022-10-26 19:57:48 +0700

    moved error handlers

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js'
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
 
@@ -16,21 +16,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
-app.use((req, res, next) => {
-	const error = new Error(`Not Found - ${req.originalUrl}`);
-	res.status(404);
-	next(error)
-});
-
-
-app.use((err, req, res, next) => {
-	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-	res.status(statusCode);
-	res.json({
-		message: err.message,
-		stack: process.env.NODE_ENV === 'production' ? null : err.stack
-	})
-})
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.rainbow.bold));
