e3e8d1cfa9200f23c6fb3f21a78aad9a06e2e749
Isreal6ix <isrealsix@gmail.com>
2022-10-26 18:05:14 +0700

    added default data and installed bcryptjs

---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs'
+const users = [
+	{
+		name: 'Admin Himself',
+		email: 'adhim@mern.com',
+		password: bcrypt.hashSync('1234', 10),
+		isAdmin: true
+	},
+	{
+		name: 'Bella perry',
+		email: 'bella@gmail.com',
+		password: bcrypt.hashSync('1234', 10),
+	},
+	{
+		name: 'Lizzy Chelovek',
+		email: 'Lizza@gmail.com',
+		password: bcrypt.hashSync('1234', 10),
+	}
+]
+
+export default users
