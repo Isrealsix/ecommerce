8accc3e05291966e68a29719c2936621c16c4e83
Isreal6ix <isrealsix@gmail.com>
2022-10-26 15:08:19 +0700

    fetched from backend and used proxy

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import products from '../products';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { Products } from '../components';
+import axios from 'axios';
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get('/api/products');
+			setProducts(data)
+		}
+		fetchProducts()
+	}, []);
+
 	return (
 		<React.Fragment>
 			<h1>Latest Products</h1>
 			<Row>
 				{products.map(product => (
-					<Col key={product._id}  sm={12} md={6} lg={4} xl={3}>
+					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 						<Products product={product} />
 					</Col>
 				))}
