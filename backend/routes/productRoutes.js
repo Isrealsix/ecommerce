ad6ff1e97d915848c52c382810d69be5b5adb2f0
Isreal6ix <isrealsix@gmail.com>
2022-10-26 20:00:31 +0700

    refactored custom error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-
 /**
  * @desc Fetch all products
  * @route GET /api/products
@@ -16,7 +15,6 @@ router.get(
 		res.json(products);
 	})
 );
-
 
 /**
  * @desc Fetch single product
@@ -31,7 +29,8 @@ router.get(
 		if (product) {
 			res.json(product);
 		} else {
-			res.status(404).json({ message: 'Product not found' });
+			res.status(404);
+			throw new Error('Product not found');
 		}
 	})
 );
