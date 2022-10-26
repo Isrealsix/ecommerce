093477081f8c492655c6fbfa33187a62bb3f489c
Isreal6ix <isrealsix@gmail.com>
2022-10-26 19:11:07 +0700

    installed and used expressAsyncHandler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,28 @@
 import express from 'express';
 const router = express.Router();
+import asyncHandler from 'express-async-handler';
+
 import Product from '../models/productModel';
 
-router.get('/', async (req, res) => {
-	const products = await Product.find({})
-	res.json(products);
-})
+router.get(
+	'/',
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	})
+);
 
-router.get('/:id', (req, res) => {
-	const product = products.find(p => p._id === req.params.id)
-	res.json(product);
-})
+router.get(
+	'/:id',
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404).json({ message: 'Product not found' });
+		}
+	})
+);
 
 export default router;
