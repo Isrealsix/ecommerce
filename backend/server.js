c5ab1e59c77c619fa6a50632354fd58989deef66
Isreal6ix <isrealsix@gmail.com>
2022-10-26 19:47:52 +0700

    added error middleware

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
+app.use((err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === 'production' ? null : err.stack
+	})
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.rainbow.bold));
