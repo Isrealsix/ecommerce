eec9c6d13b1ffbf7ff0f5934e90c45acc83acbb9
Isreal6ix <isrealsix@gmail.com>
2022-10-26 19:05:06 +0700

    created routes folder and moved products

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js'
+
+
 const app = express();
 
 dotenv.config();
@@ -12,14 +14,8 @@ app.get('/', (req, res) => {
 	res.send('API online')
 })
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find(p => p._id === req.params.id)
-	res.json(product);
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.rainbow.bold));
