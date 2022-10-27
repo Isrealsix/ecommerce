8de6143ec93d600e23b71cd97b1416588a54ed60
Isreal6ix <isrealsix@gmail.com>
2022-10-28 01:41:43 +0700

    added quantity dropdown select

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form,
+} from 'react-bootstrap';
 import { listProductDetails } from '../actions';
 import { Loader, Message, Rating } from '../components';
 
 const ProductScreen = () => {
+	const [qty, setQty] = useState(0);
 	const { id } = useParams();
 	const dispatch = useDispatch();
 
@@ -67,6 +76,27 @@ const ProductScreen = () => {
 										</Col>
 									</Row>
 								</ListGroup.Item>
+
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as="select"
+													value={qty}
+													onChange={e => setQty(e.target.value)}
+												>
+													{[...Array(product.countInStock).keys()].map(x => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 								<ListGroup.Item>
 									<Button
 										className="btn-block"
