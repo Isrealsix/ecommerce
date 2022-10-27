8330798c8b59b3864338e97c8e0e1dae63da621d
Isreal6ix <isrealsix@gmail.com>
2022-10-28 00:40:12 +0700

    dispatched action and used useSelector

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,1 @@
+export { listProducts } from './productActions';
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import { Products } from '../components';
-import axios from 'axios';
+import { listProducts } from '../actions';
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+	const productList = useSelector(state => state.productList);
+	const { loading, error, products } = productList;
+
 	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products');
-			setProducts(data)
-		}
-		fetchProducts()
-	}, []);
+		dispatch(listProducts());
+	}, [dispatch]);
 
 	return (
 		<React.Fragment>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map(product => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Products product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<h3>{error}</h3>
+			) : (
+				<Row>
+					{products.map(product => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Products product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</React.Fragment>
 	);
 };
