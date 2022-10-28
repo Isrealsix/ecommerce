415064c3d330bb0566f13efab829dffffdb27a6b
Isreal6ix <isrealsix@gmail.com>
2022-10-28 20:20:00 +0700

    added await on authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
 	
 	const user = await User.findOne({ email });
 
-	if (user && (user.matchPassword(password))) {
+	if (user && (await user.matchPassword(password))) {
 		res.json({
 			_id: user._id,
 			name: user.name,
