eaddb0d9b34a0684ea950f2dba0f2320be71d676
Isreal6ix <isrealsix@gmail.com>
2022-10-29 23:00:52 +0700

    connected userRegisterReducer with combineReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,3 +1,3 @@
 export { productListReducer, productDetailsReducer } from './productReducers';
 export { cartReducer } from './cartReducers';
-export { userLoginReducer } from './userReducers';
+export { userLoginReducer, userRegisterReducer } from './userReducers';
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,26 +1,37 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { productListReducer, productDetailsReducer, cartReducer, userLoginReducer } from './reducers';
+import {
+	productListReducer,
+	productDetailsReducer,
+	cartReducer,
+	userLoginReducer,
+	userRegisterReducer,
+} from './reducers';
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
-	userLogin: userLoginReducer
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer
 });
 
-const cartItemsLS = localStorage.getItem('cartItems')
+const cartItemsLS = localStorage.getItem('cartItems');
 const cartItemsFromStorage = cartItemsLS ? JSON.parse(cartItemsLS) : [];
 
-const userInfoLS = localStorage.getItem('userInfo')
+const userInfoLS = localStorage.getItem('userInfo');
 const userInfoFromStorage = userInfoLS ? JSON.parse(cartItemsLS) : null;
 
 const initialState = {
 	cart: { cartItems: cartItemsFromStorage },
-	userLogin: { userInfo: userInfoFromStorage}
+	userLogin: { userInfo: userInfoFromStorage },
 };
-const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const middleware = [thunk];
+const store = createStore(
+	reducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
