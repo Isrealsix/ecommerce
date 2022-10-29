66af6de8f4bcd3ea9f70b29a848bf44f8c952405
Isreal6ix <isrealsix@gmail.com>
2022-10-29 15:18:16 +0700

    added getUserProfile route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,4 +26,14 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser }
+
+/**
+ * @desc  Get user profile
+ * @route GET /api/users/profile
+ * @access Private
+ */
+ const getUserProfile = asyncHandler(async (req, res) => {
+	res.send('Success')
+});
+
+export { authUser, getUserProfile }
