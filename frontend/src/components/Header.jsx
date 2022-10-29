07cc2aaa0856ae1fd910ce737471071add27a056
Isreal6ix <isrealsix@gmail.com>
2022-10-29 22:50:27 +0700

    dispatched logout fnc

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { logout } from '../actions';
 
 const Header = () => {
 	const dispatch = useDispatch();
 	const userLogin = useSelector(state => state.userLogin);
 	const { userInfo } = userLogin;
 	const logoutHandler = () => {
-
+		dispatch(logout())
 	}
 	return (
 		<header>
