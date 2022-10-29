449600dc6e0114d30cf310230acaf40871a4e41d
Isreal6ix <isrealsix@gmail.com>
2022-10-29 18:49:55 +0700

    Created loginScreen 'Screen'

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Products } from './Products';
 export { default as Rating } from './Rating';
 export { default as Loader } from './Loader';
 export { default as Message } from './Message';
+export { default as FormContainer } from './FormContainer';
