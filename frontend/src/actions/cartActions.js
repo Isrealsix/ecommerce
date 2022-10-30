64d133606a65a5caae93d9f2c5b2909e0fda5d85
Isreal6ix <isrealsix@gmail.com>
2022-10-30 12:38:10 +0700

    added shipping address reducer

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
 	const { data } = await axios.get(`/api/products/${id}`);
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants';
+import {
+	CART_ADD_ITEM,
+	CART_REMOVE_ITEM,
+	CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
 	switch (action.type) {
@@ -9,20 +13,29 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 			if (itemExist) {
 				return {
 					...state,
-					cartItems: state.cartItems.map(x => x.product === itemExist.product ? item : x)
-				}
+					cartItems: state.cartItems.map(x =>
+						x.product === itemExist.product ? item : x
+					),
+				};
 			} else {
 				return {
 					...state,
-					cartItems: [...state.cartItems, item]
-				}
+					cartItems: [...state.cartItems, item],
+				};
 			}
 		case CART_REMOVE_ITEM:
 			return {
 				...state,
-				cartItems: state.cartItems.filter(item => item.product !== action.payload)
+				cartItems: state.cartItems.filter(
+					item => item.product !== action.payload
+				),
+			};
+		case CART_SAVE_SHIPPING_ADDRESS:
+			return {
+				...state,
+				shippingAddress: action.payload
 			}
 		default:
-			return state
+			return state;
 	}
-}
+};
