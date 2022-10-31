3a9d025e908a2b075b82be546fc364b74a6d9d72
Isreal6ix <isrealsix@gmail.com>
2022-10-31 19:11:11 +0700

    dispatched createOrder

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Message, CheckoutSteps } from '../components';
+import { createOrder } from '../actions';
 
 const PlaceOrderScreen = () => {
 	const cart = useSelector(state => state.cart);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	const addDecimals = num => {
 		return (Math.round(num * 100) / 100).toFixed(2);
@@ -26,8 +29,28 @@ const PlaceOrderScreen = () => {
 		Number(cart.taxPrice)
 	).toFixed(2);
 
+	const orderCreate = useSelector(state => state.orderCreate);
+	const { order, success, error } = orderCreate;
+
+	useEffect(() => {
+		if (success) {
+			navigate(`/order/${order._id}`);
+		}
+		// eslint-disable-next-line
+	}, [success, navigate]);
+
 	const placeOrderHandler = () => {
-		console.log('order');
+		dispatch(
+			createOrder({
+				orderItems: cart.cartItems,
+				shippingAddress: cart.shippingAddress,
+				paymentMethod: cart.paymentMethod,
+				itemsPrice: cart.itemsPrice,
+				shippingPrice: cart.shippingPrice,
+				taxPrice: cart.taxPrice,
+				totalPrice: cart.totalPrice,
+			})
+		);
 	};
 	return (
 		<>
@@ -113,6 +136,9 @@ const PlaceOrderScreen = () => {
 									<Col>Total</Col>
 									<Col>${cart.totalPrice}</Col>
 								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								{error && <Message variant="danger">{error}</Message>}
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Button
