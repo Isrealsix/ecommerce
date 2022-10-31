6e08252820e75371866f036f9bba6fcc8cfdc7a8
Isreal6ix <isrealsix@gmail.com>
2022-10-31 16:36:09 +0700

    added reducer case for CART_SAVE_PAYMENT_METHOD

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,9 +2,13 @@ import {
 	CART_ADD_ITEM,
 	CART_REMOVE_ITEM,
 	CART_SAVE_SHIPPING_ADDRESS,
+	CART_SAVE_PAYMENT_METHOD,
 } from '../constants';
 
-export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
+export const cartReducer = (
+	state = { cartItems: [], shippingAddress: {} },
+	action
+) => {
 	switch (action.type) {
 		case CART_ADD_ITEM:
 			const item = action.payload;
@@ -33,8 +37,13 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
 		case CART_SAVE_SHIPPING_ADDRESS:
 			return {
 				...state,
-				shippingAddress: action.payload
-			}
+				shippingAddress: action.payload,
+			};
+		case CART_SAVE_PAYMENT_METHOD:
+			return {
+				...state,
+				paymentMethod: action.payload,
+			};
 		default:
 			return state;
 	}
