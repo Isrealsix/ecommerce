b742e44bd91fa7d39d599c8f61113159636c6187
Isreal6ix <isrealsix@gmail.com>
2022-11-01 21:33:45 +0700

    completed Order screen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -12,6 +12,17 @@ const OrderScreen = () => {
 	const orderDetails = useSelector(state => state.orderDetails);
 	const { order, loading, error } = orderDetails;
 
+	if (!loading) {
+		const addDecimals = num => {
+			return (Math.round(num * 100) / 100).toFixed(2);
+		};
+
+		// calculate prices
+		order.itemsPrice = addDecimals(
+			order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+		);
+	}
+
 	useEffect(() => {
 		dispatch(getOrderDetails(orderId));
 	}, [dispatch, orderId]);
@@ -30,17 +41,39 @@ const OrderScreen = () => {
 						<ListGroup.Item>
 							<h2>Shipping</h2>
 							<p>
+								<strong>Name: </strong>
+								{order.user.name}
+							</p>{' '}
+							<p>
+								<strong>Email: </strong>
+								<a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+							</p>
+							<p>
 								<strong>Address: </strong>
 								{order.shippingAddress.address}, {order.shippingAddress.city}{' '}
 								{order.shippingAddress.postalCode},{' '}
 								{order.shippingAddress.country}
 							</p>
+							{order.isDelivered ? (
+								<Message variant="success">
+									Delivered on {order.deliveredAt}
+								</Message>
+							) : (
+								<Message variant="danger">Not Delivered</Message>
+							)}
 						</ListGroup.Item>
 
 						<ListGroup.Item>
 							<h2>Payment Method</h2>
-							<strong>Method: </strong>
-							{order.paymentMethod}
+							<p>
+								<strong>Method: </strong>
+								{order.paymentMethod}
+							</p>
+							{order.isPaid ? (
+								<Message variant="success">Paid on {order.paidAt}</Message>
+							) : (
+								<Message variant="danger">Not Paid</Message>
+							)}
 						</ListGroup.Item>
 
 						<ListGroup.Item>
