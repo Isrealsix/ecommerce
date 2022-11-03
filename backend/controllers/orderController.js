4fa9e65217206a0d1417cb4487797c4f5130e9e3
Isreal6ix <isrealsix@gmail.com>
2022-11-03 19:22:33 +0700

    added controller to get user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,4 +84,15 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+/**
+ * @desc Get Authenticated user orders
+ * @route GET /api/orders/orders
+ * @access Private
+ */
+const getMyOrders = asyncHandler(async (req, res) => {
+	const orders = await Order.find({ user: req.user._id });
+
+	res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
