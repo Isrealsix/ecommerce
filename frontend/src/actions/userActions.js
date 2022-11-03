ad9381a63ae789c771cd24cf696e7edd51d5c11b
Isreal6ix <isrealsix@gmail.com>
2022-11-03 23:00:40 +0700

    dispatched USER_LIST_RESET on logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -18,6 +18,7 @@ import {
 	USER_LIST_REQUEST,
 	USER_LIST_SUCCESS,
 	USER_LIST_FAIL,
+	USER_LIST_RESET,
 } from '../constants';
 
 export const login = (email, password) => async dispatch => {
@@ -57,9 +58,10 @@ export const login = (email, password) => async dispatch => {
 
 export const logout = () => async dispatch => {
 	localStorage.removeItem('userInfo');
+	dispatch({ type: USER_LOGOUT });
 	dispatch({ type: USER_DETAILS_RESET });
 	dispatch({ type: ORDER_LIST_MY_RESET });
-	dispatch({ type: USER_LOGOUT });
+	dispatch({ type: USER_LIST_RESET });
 };
 
 export const register = (name, email, password) => async dispatch => {
