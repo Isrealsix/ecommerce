5da02fb1b34761984e5c4d3765e0a7fd6c42c29f
Isreal6ix <isrealsix@gmail.com>
2022-11-03 22:56:01 +0700

    redirect from admin panel if not authorized

--- frontend/src/screens/UserListScreen.jsx
+++ frontend/src/screens/UserListScreen.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Table, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,14 +8,21 @@ import { listUsers } from '../actions';
 
 const UserListScreen = () => {
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const userList = useSelector(state => state.userList);
 	const { loading, error, users } = userList;
 
+	const userLogin = useSelector(state => state.userLogin);
+	const { userInfo } = userLogin;
 	useEffect(() => {
-		dispatch(listUsers());
-	}, [dispatch]);
+		if (userInfo && userInfo.isAdmin) {
+			dispatch(listUsers());
+		} else {
+			navigate('/login');
+		}
+	}, [dispatch, navigate, userInfo]);
 
-	const deleteHandler = (id) => { };
+	const deleteHandler = id => {};
 
 	return (
 		<>
