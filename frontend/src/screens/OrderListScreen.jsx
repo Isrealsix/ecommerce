490f9306c70650deb3f1b5eb0a16c3e3a6b06879
Isreal6ix <isrealsix@gmail.com>
2022-11-06 02:10:01 +0700

    completed order screen

--- frontend/src/screens/OrderListScreen.jsx
+++ frontend/src/screens/OrderListScreen.jsx
@@ -64,7 +64,7 @@ const OrderListScreen = () => {
 									)}
 								</td>
 								<td>
-									<LinkContainer to={`/admin/order/${order._id}/edit`}>
+									<LinkContainer to={`/order/${order._id}`}>
 										<Button variant="light" className="btn-sm">
 											Details
 										</Button>
