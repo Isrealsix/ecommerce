78d002ff6f52f4afe767c55691decbcf3d9c5b1c
Isreal6ix <isrealsix@gmail.com>
2022-11-05 22:38:20 +0700

    implemented upload controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,4 @@
 export { default as productRoutes } from './productRoutes.js';
 export { default as userRoutes } from './userRoutes.js';
 export { default as orderRoutes } from './orderRoutes.js';
+export { default as uploadRoutes } from './uploadRoutes.js'
