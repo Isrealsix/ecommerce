3ecef282bab436a3a49fca473ed26f600a6bcaad
Isreal6ix <isrealsix@gmail.com>
2022-11-05 20:08:18 +0700

    added productCreateReducer to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -17,6 +17,7 @@ import {
 	userDeleteReducer,
 	userUpdateReducer,
 	productDeleteReducer,
+	productCreateReducer,
 } from './reducers';
 
 const reducer = combineReducers({
@@ -35,6 +36,7 @@ const reducer = combineReducers({
 	orderDetails: orderDetailsReducer,
 	orderPay: orderPayReducer,
 	orderListMy: orderListMyReducer,
+	productCreate: productCreateReducer,
 });
 
 const cartItemsLS = localStorage.getItem('cartItems');
