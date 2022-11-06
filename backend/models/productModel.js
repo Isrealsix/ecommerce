f255622c23396d2c717a209d53c2836f3c795cf4
Isreal6ix <isrealsix@gmail.com>
2022-11-06 16:48:10 +0700

    added user to product review controller

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,18 +1,26 @@
 import mongoose from 'mongoose';
-const reviewSchema = mongoose.Schema({
-	name: { type: String, required: true },
-	rating: { type: String, required: true },
-	comment: { type: String, required: true }
-}, {
-	timestamps: true
-});
+const reviewSchema = mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		rating: { type: String, required: true },
+		comment: { type: String, required: true },
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const productSchema = mongoose.Schema(
 	{
 		user: {
 			type: mongoose.Schema.Types.ObjectId,
 			required: true,
-			ref: 'User'
+			ref: 'User',
 		},
 		name: {
 			type: String,
@@ -38,23 +46,23 @@ const productSchema = mongoose.Schema(
 		rating: {
 			type: Number,
 			required: true,
-			default: 0
+			default: 0,
 		},
 		numReviews: {
 			type: Number,
 			required: true,
-			default: 0
+			default: 0,
 		},
 		price: {
 			type: Number,
 			required: true,
-			default: 0
+			default: 0,
 		},
 		countInStock: {
 			type: Number,
 			required: true,
-			default: 0
-		}
+			default: 0,
+		},
 	},
 	{ timestamps: true }
 );
