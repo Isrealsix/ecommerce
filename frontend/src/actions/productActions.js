07876a0e583e374266ddb2fd60702d1835c010c2
Isreal6ix <isrealsix@gmail.com>
2022-11-08 17:55:53 +0700

    added pageNumber parameter in list products action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,25 +20,29 @@ import {
 	PRODUCT_CREATE_REVIEW_FAIL,
 } from '../constants';
 
-export const listProducts = (keyword = '') => async dispatch => {
-	try {
-		dispatch({ type: PRODUCT_LIST_REQUEST });
+export const listProducts =
+	(keyword = '', pageNumber = '') =>
+	async dispatch => {
+		try {
+			dispatch({ type: PRODUCT_LIST_REQUEST });
 
-		const { data } = await axios.get(`/api/products?keyword=${keyword}`);
-		dispatch({
-			type: PRODUCT_LIST_SUCCESS,
-			payload: data,
-		});
-	} catch (error) {
-		dispatch({
-			type: PRODUCT_LIST_FAIL,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		});
-	}
-};
+			const { data } = await axios.get(
+				`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+			);
+			dispatch({
+				type: PRODUCT_LIST_SUCCESS,
+				payload: data,
+			});
+		} catch (error) {
+			dispatch({
+				type: PRODUCT_LIST_FAIL,
+				payload:
+					error.response && error.response.data.message
+						? error.response.data.message
+						: error.message,
+			});
+		}
+	};
 
 export const listProductDetails = id => async dispatch => {
 	try {
@@ -182,11 +186,7 @@ export const createProductReview =
 				},
 			};
 
-			await axios.post(
-				`/api/products/${productId}/reviews`,
-				review,
-				config
-			);
+			await axios.post(`/api/products/${productId}/reviews`, review, config);
 
 			dispatch({
 				type: PRODUCT_CREATE_REVIEW_SUCCESS,
